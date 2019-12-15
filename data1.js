a6be2ffd5cbe766363e571a929b51ab2f0099e2b
Seth Whitaker <sdw3489@gmail.com>
2019-12-15 15:36:07 -0700

    undiecar

---
+++ data1.js
@@ -0,0 +1,87 @@
+sessionName=Session+Name
+&sessionPassword=
+&weatherType=1
+&weatherTemp=78
+&weatherTempChoice=0
+&weatherHumidity=55
+&weatherFog=0
+&weatherWindSpeed=2
+&weatherWindSpeedChoice=0
+&weatherWindDirection=0
+&weatherCloudCover=1
+&sessionDur=1
+&startAt=1545960000000
+&rcdPractice=3600
+&rcdQual=0
+&rcdRace=
+&farmId=11
+&maxNumDrivers=60
+&trackId=330
+&timeOfDay=0
+&simulatedStartTime=
+&earthSpeedup=1
+&simulatedTimeOffsets=
+&useMetricMeasurement=0
+&multiClass=0
+// fixedSetup
+&carId0=4
+&maxPctFuelFillId0=100
+&weightPenaltyId0=0
+&carSetupRaceId0=73991
+&carSetupQualId0=undefined
+// multiClassGridTogetherRadio
+// sessionType
+&carClassId0=0
+&carClassId1=0
+&carClassId2=0
+&carClassId3=0
+&carClassId4=0
+&carClassId5=0
+&sessionType=1
+&numServers=1
+&isFinal=0
+&practiceLength=60
+// qualLengthLaps
+// qualLengthMins
+&raceLength=0
+&raceLengthType=2
+&numJokerLaps=0
+&gridId=
+&rootId=
+&parentId=
+&tourneyAction=new
+&startType=R
+&numFastTows=1
+&fullCourseCautions_ignore=2
+&fullCourseCautions=1
+&luckyDog=1
+&restartType=2
+&leaveMarbles=1
+&rubberLevelPractice=-1
+// rubberLevelQualify
+// rubberLevelRace
+// driverChanges
+&minTeamDrivers=1
+&maxTeamDrivers=2
+// qualifierMustStartRace
+&driverChangeRule=0
+&sessionDesc=Undiecar+test
+// maxIncidentsCheckbox
+&maxIncidents=
+&irCheckbox=on
+&minIR=500
+&maxIR=10000
+&srCheckbox=on
+&minLicLevel=6
+&maxLicLevel=28
+// clubCheckbox
+// leagueCheckbox
+&sessionAdmins=279455 %2C 223698
+&recentSelect=223698
+&fsSelect=110963
+&memberName=%5Benter+search+term%5D
+&hardcoreLevel=1
+// restrictResults
+// restrictViewing
+&paceCarId=0
+&isTourney=0
